3fb4a4846e9825942e8b9149134c54273a87ca42
Kavyanjali Karan <karankavyanjali77@gmail.com>
2026-03-16 17:32:29 +0530

    Delete load_pdf.py

--- load_pdf.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader("data/book.pdf")
-
-documents = loader.load()
-
-print(len(documents))
-print(documents[0].page_content)
